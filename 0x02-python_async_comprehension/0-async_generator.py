e9dcbfffe7cbce8426e8f7bd1e6c876256292a41
PrincessRuth90 <marcusruth86@gmail.com>
2023-04-04 19:03:49 +0000

    fix pycodestyle

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,13 +6,11 @@ import asyncio
 from typing import Generator
 import random
 
+
 async def async_generator() -> Generator[float, None, None]:
-   """
-   The coroutine will loop 10 times, each time asynchronously wait 1 second
-   """
-
-   for _ in range(10):
-       await asyncio.sleep(1)
-       yield random.random() * 10
-
-
+    """
+    The coroutine will loop 10 times
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
